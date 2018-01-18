a72af810ef06f5ef05ab67f65f406f68d7516b3d
dan98 <s3135527@peregrine.hpc.rug.nl>
2018-01-19 00:38:46 +0100

    video

--- frames2video.m
+++ frames2video.m
@@ -1,6 +1,9 @@
 function frames2video(frames, filename)
 	nr = length(frames);
-	v = VideoWriter(filename, 'FileFormat', 'mp4', 'Quality', 100, 'FrameRate', nr/10);
+	v = VideoWriter(filename);
+	v.FileFormat = 'mp4';
+	v.Quality = 100;
+	v.FrameRate =nr/10;
 	open(v);
 
 	for i=1:length(frames)
